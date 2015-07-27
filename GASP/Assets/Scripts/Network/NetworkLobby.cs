498f36a31b32541f7bf391fd06942e1ffb527ba9
Ryan <rthill91@gmail.com>
2015-07-26 20:11:16 -0500

    added title screen

---
+++ GASP/Assets/Scripts/Network/NetworkLobby.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NetworkLobby : MonoBehaviour {
+
+	const string VERSION = "V0.6.0";
+	private RoomInfo[] roomsList;
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings (VERSION);
+	}
+
+	void OnGUI()
+	{
+		// Title
+		GUIStyle titleStyle = new GUIStyle ();
+		titleStyle.fontSize = 30;
+		titleStyle.normal.textColor = Color.white;
+		GUI.Label (new Rect (Screen.width / 2, 20, 100, 50), "GASP", titleStyle);
+
+		if (!PhotonNetwork.connected) {
+			GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+		} else if (PhotonNetwork.room == null) {
+			// Room Name
+			ApplicationModel.roomName = GUI.TextField (new Rect (100, 100, 250, 30), ApplicationModel.roomName);
+			// Player Name
+			//ApplicationModel.playerName = GUI.TextField (new Rect (400, 100, 250, 30), ApplicationModel.playerName);
+			// Join Server Button
+			if (GUI.Button (new Rect (100, 150, 250, 50), "Start Server")) {
+				//PhotonNetwork.CreateRoom (ApplicationModel.roomName, true, true, 4);
+				Application.LoadLevel("MainScene");
+			}
+			// Rooms List
+			if (roomsList != null) {
+				for (int i = 0; i < roomsList.Length; i++) {
+					if (GUI.Button (new Rect (100, 250 + (110 * i), 250, 30), "Join " + roomsList [i].name)) {
+						//PhotonNetwork.JoinRoom (roomsList [i].name);
+						Application.LoadLevel("MainScene");
+					}
+				}
+			}
+		}
+	}
+
+	void OnReceivedRoomListUpdate() {
+		roomsList = PhotonNetwork.GetRoomList ();
+	}
+	/*
+	void OnJoinedRoom() {
+		Debug.Log ("Connected to Room");
+		Application.LoadLevel ("MainScene");
+	}
+	*/
+}
--- GASP/Assets/Scripts/Network/NetworkManager.cs
+++ GASP/Assets/Scripts/Network/NetworkManager.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
 
-	const string VERSION = "V0.0.2";
-
-	public string roomName = "room1";
 	public string playerPrefabName = "Player";
 	private Transform spawnPoint;
 	public GameObject[] spawnPoints;
@@ -14,23 +11,37 @@ public class NetworkManager : MonoBehaviour {
 	void Start () {
 		spawnPoints = GameObject.FindGameObjectsWithTag ("Respawn");
 
-		PhotonNetwork.ConnectUsingSettings(VERSION);
-	}
-
-	void OnJoinedLobby() {
-		RoomOptions roomOptions = new RoomOptions() {
-			isVisible = false,
+		RoomOptions roomOptions = new RoomOptions () {
+			isOpen = true,
+			isVisible = true,
 			maxPlayers = 4
 		};
-		PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+
+		PhotonNetwork.JoinOrCreateRoom (ApplicationModel.roomName, roomOptions, TypedLobby.Default);
+		/*
+		if (PhotonNetwork.connected) {
+			spawnPoint = spawnPoints [PhotonNetwork.playerList.Length - 1].transform;
+
+			PhotonNetwork.Instantiate(playerPrefabName,
+			                          spawnPoint.position,
+			                          Quaternion.identity,
+			                          0);
+
+		}
+		*/
 	}
-	
+
 	void OnJoinedRoom() {
 		spawnPoint = spawnPoints [PhotonNetwork.playerList.Length - 1].transform;
-
-		PhotonNetwork.Instantiate(playerPrefabName,
+		
+		PhotonNetwork.Instantiate (playerPrefabName,
 		                          spawnPoint.position,
 		                          Quaternion.identity,
 		                          0);
+	}
+
+	void OnGUI() {
+		if(PhotonNetwork.room != null)
+			GUILayout.Label (PhotonNetwork.room.name);
 	}
 }
